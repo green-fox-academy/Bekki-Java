5a2cb8f0e76409e47d264aa511b9bb77dbdd3b9b
Bekhzod Ismoilov <bekkiismailov@gmail.com>
2020-07-26 15:45:05 +0200

    First Repository GFA

---
+++ HelloOthers.java
@@ -0,0 +1,5 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+    String [] HelloOthers {"Hello Esther!","Hello Mary!","Hello Joe!",
+    }
+}
---
+++ IntroduceYourself.java
@@ -0,0 +1,15 @@
+package com.bekki;
+
+import static java.lang.System.*;
+
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+        String name = "Bekki";
+        int age = 24;
+        double height = 1.85d;
+        System.out.println(name);
+        out.println(age);
+        out.println(height);
+    }
+}
